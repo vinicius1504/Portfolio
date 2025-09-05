e96d0ee797fb03adc7651ad4422208a6611b4709
Vini Leite <70609265+vinicius1504@users.noreply.github.com>
2025-09-05 09:07:10 -0400

    feat: update icons and improve container layout
    - Added new SVG icons for Python, React, TypeScript, and Vite.
    - Removed old SVG icons for Python and TypeScript.
    - Updated import paths for icons in Container2Dynamic component.
    - Modified experience entries in English and Portuguese localization files.
    - Adjusted CSS for mobile container to improve layout and animation speed.

--- portfolio-trotta-main/src/components/containers/Container2/Container2Dynamic.jsx
+++ portfolio-trotta-main/src/components/containers/Container2/Container2Dynamic.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { useLanguage } from '../../../contexts/LanguageContext/LanguageContext.jsx';
-import htmlIcon from '../../../assets/images/icons/html-icon.svg';
-import cssIcon from '../../../assets/images/icons/css-icon.svg';
-import jsIcon from '../../../assets/images/icons/js-icon.svg';
-import angularIcon from '../../../assets/images/icons/angularjs.svg';
-import typescriptIcon from '../../../assets/images/icons/typescript-icon.svg';
-import nextjsIcon from '../../../assets/images/icons/nextjs-icon.svg';
-import nodejsIcon from '../../../assets/images/icons/nodejs-icon.svg';
-import javaIcon from '../../../assets/images/icons/java-icon.svg';
-import mongoIcon from '../../../assets/images/icons/mongo-icon.svg';
-import gitIcon from '../../../assets/images/icons/git-icon.svg';
-import PythonIcon from '../../../assets/images/icons/python.svg';
-import PHPIcon from '../../../assets/images/icons/php.svg';
+import htmlIcon from '../../../assets/images/icons/html-svgrepo-com.svg';
+import cssIcon from '../../../assets/images/icons/css-3-svgrepo-com.svg';
+import jsIcon from '../../../assets/images/icons/js-svgrepo-com.svg';
+import angularIcon from '../../../assets/images/icons/angular-svgrepo-com.svg';
+import typescriptIcon from '../../../assets/images/icons/typescript-icon-svgrepo-com.svg';
+import reactIcon from '../../../assets/images/icons/react-svgrepo-com.svg';
+import nodejsIcon from '../../../assets/images/icons/node-svgrepo-com.svg';
+import javaIcon from '../../../assets/images/icons/java-svgrepo-com.svg';
+import mongoIcon from '../../../assets/images/icons/mongodb-svgrepo-com.svg';
+import gitIcon from '../../../assets/images/icons/git-svgrepo-com.svg';
+import pythonIcon from '../../../assets/images/icons/python-svgrepo-com.svg';
+import phpIcon from '../../../assets/images/icons/php2-svgrepo-com.svg';
+import viteIcon from '../../../assets/images/icons/vite-svgrepo-com.svg';
 
 const Container2Dynamic = () => {
     const { t } = useLanguage();
@@ -41,11 +42,11 @@ const Container2Dynamic = () => {
                             <span>TypeScript</span>
                         </div>
                         <div className="slide">
-                            <img src={PythonIcon} alt="Python" />
+                            <img src={pythonIcon} alt="Python" />
                             <span>Python</span>
                         </div>
                         <div className="slide">
-                            <img src={PHPIcon} alt="PHP" />
+                            <img src={phpIcon} alt="PHP" />
                             <span>PHP</span>
                         </div>
                         <div className="slide">
@@ -61,8 +62,8 @@ const Container2Dynamic = () => {
                             <span>Angular</span>
                         </div>
                         <div className="slide">
-                            <img src={nextjsIcon} alt="Next.js" />
-                            <span>Next JS</span>
+                            <img src={reactIcon} alt="React" />
+                            <span>React</span>
                         </div>
                         <div className="slide">
                             <img src={nodejsIcon} alt="Node.js" />
@@ -73,21 +74,7 @@ const Container2Dynamic = () => {
                             <span>Mongo DB</span>
                         </div>
                         <div className="slide">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 256 257">
-                                <defs>
-                                    <linearGradient id="logosVitejs0" x1="-.828%" x2="57.636%" y1="7.652%" y2="78.411%">
-                                        <stop offset="0%" stopColor="#Fff" />
-                                        <stop offset="100%" stopColor="gray" />
-                                    </linearGradient>
-                                    <linearGradient id="logosVitejs1" x1="43.376%" x2="50.316%" y1="2.242%" y2="89.03%">
-                                        <stop offset="0%" stopColor="#FFEA83" />
-                                        <stop offset="8.333%" stopColor="#FFF" />
-                                        <stop offset="100%" stopColor="#FFF" />
-                                    </linearGradient>
-                                </defs>
-                                <path fill="url(#logosVitejs0)" d="M255.153 37.938L134.897 252.976c-2.483 4.44-8.862 4.466-11.382.048L.875 37.958c-2.746-4.814 1.371-10.646 6.827-9.67l120.385 21.517a6.537 6.537 0 0 0 2.322-.004l117.867-21.483c5.438-.991 9.574 4.796 6.877 9.62Z" />
-                                <path fill="url(#logosVitejs1)" d="M185.432.063L96.44 17.501a3.268 3.268 0 0 0-2.634 3.014l-5.474 92.456a3.268 3.268 0 0 0 3.997 3.378l24.777-5.718c2.318-.535 4.413 1.507 3.936 3.838l-7.361 36.047c-.495 2.426 1.782 4.5 4.151 3.78l15.304-4.649c2.372-.72 4.652 1.36 4.15 3.788l-11.698 56.621c-.732 3.542 3.979 5.473 5.943 2.437l1.313-2.028l72.516-144.72c1.215-2.423-.88-5.186-3.54-4.672l-25.505 4.922c-2.396.462-4.435-1.77-3.759-4.114l16.646-57.705c.677-2.35-1.37-4.583-3.769-4.113Z" />
-                            </svg>
+                            <img src={viteIcon} alt="Vite" />
                             <span>Vite</span>
                         </div>
                         <div className="slide">
